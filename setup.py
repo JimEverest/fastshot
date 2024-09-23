87d50ced2f3ddf7e887d6e7085bfaecb04c0c68e
jimeverest <tianwai263@gmail.com>
2024-09-23 22:14:56 +0800

    1.0.1 update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastshot',
-    version='0.1.51',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
