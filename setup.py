1fceb402259dc46d13b4b89bb8b1bf054aa74919
jimeverest <tianwai263@gmail.com>
2024-09-23 22:34:42 +0800

    Update version to 1.0.2 and ensure config.ini is included

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastshot',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -17,6 +17,9 @@ setup(
         "paddleocr",
         "paddlepaddle"
     ],
+    package_data={
+        'fastshot': ['config.ini'],  # 明确指示包含 config.ini
+    },
     entry_points={
         'console_scripts': [
             'fastshot = fastshot.main:main',
